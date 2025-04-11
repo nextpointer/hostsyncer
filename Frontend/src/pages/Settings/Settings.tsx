c5b0b64f65880b38b6a843a90cc1568a2edd7c99
lazy34 <maitysurajit0901@gmail.com>
2025-04-11 12:50:38 +0530

    setting page config stucture complete

--- Frontend/src/pages/Settings/Settings.tsx
+++ Frontend/src/pages/Settings/Settings.tsx
@@ -20,16 +20,19 @@ export const Settings = () => {
     setTheme(target.value);
   };
   // handle and edit access key
-  const handleAccessKey = ()=>{
+  const handleAccessKey = () => {
     setKeyButtonClicked(true);
-  }
+  };
 
   return (
     <>
       <div class="setting-header">
         <h1 class="setting-h1">Settings</h1>
       </div>
+      {/* start the container */}
       <div class="setting-container">
+        <h2>Basic Configuration</h2>
+        {/* general config setting */}
         <div class="theme-config basic-config">
           <span>Theme</span>
           <select value={theme()} onChange={handleThemeChange}>
@@ -38,6 +41,27 @@ export const Settings = () => {
             <option value="system">System</option>
           </select>
         </div>
+        {/* port config */}
+        <div class="port-config basic-config">
+          <span>master-port</span>
+          <input type="text" class="config-input port-input" value={"5432"} />
+        </div>
+        {/* ip config */}
+        <div class="ip-config basic-config">
+          <span>master-ip</span>
+          <input type="text" class="config-input" value={"255.255.255.255"} />
+        </div>
+        {/* access key token config */}
+        <div class="basic-config">
+          <span>Master node API access key</span>
+          <input
+            class="setting-access-key-input config-input"
+            value={"dffd-dnd54d-dd454d-dmjnn-22mddd"}
+            type="text"
+          />
+        </div>
+        {/* master node config */}
+        <h2>Advance Configuration</h2>
         <div class="master-node-confirmation basic-config">
           <span>Use master node's config</span>
           <ToggleButton
@@ -45,6 +69,7 @@ export const Settings = () => {
             handleToggle={handleToggle}
           />
         </div>
+        {/* if master node is true show this containers */}
         <div
           class={`if-master-true ${
             !IsMasterNodeConfig() ? "opacity-down" : ""
@@ -60,32 +85,6 @@ export const Settings = () => {
                 value={"5000"}
               />
               <span>ms</span>
-            </div>
-          </div>
-          <div class="basic-config">
-            <span>Master node API access key</span>
-            <div class={`setting-access-key-container basic-config-container ${isKeyButtonClicked()?"key-transition":""}`} onfocusout={()=>setKeyButtonClicked(false)}>
-              <textarea
-                class="setting-access-key-input config-input"
-                value={"xxxx"}
-                disabled={!IsMasterNodeConfig()}
-              />
-              <button disabled={!IsMasterNodeConfig()} onclick={handleAccessKey}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <g fill="none" stroke="black" stroke-width="1.5">
-                    <path d="m15.287 3.152l-.927.927l-8.521 8.52c-.577.578-.866.867-1.114 1.185a6.6 6.6 0 0 0-.749 1.211c-.173.364-.302.752-.56 1.526l-1.094 3.281l-.268.802a1.06 1.06 0 0 0 1.342 1.342l.802-.268l3.281-1.094c.775-.258 1.162-.387 1.526-.56q.647-.308 1.211-.749c.318-.248.607-.537 1.184-1.114l8.521-8.521l.927-.927a3.932 3.932 0 0 0-5.561-5.561Z" />
-                    <path
-                      d="M14.36 4.078s.116 1.97 1.854 3.708s3.707 1.853 3.707 1.853M4.198 21.678l-1.876-1.876"
-                      opacity="0.5"
-                    />
-                  </g>
-                </svg>
-              </button>
             </div>
           </div>
         </div>
